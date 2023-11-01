1a9b19bf11a8aea446905b7ef74efdd869eee494
Luiz Parente <luiz_parente@mcafee.com>
2023-10-31 20:43:41 -0400

    Intro to WPF.

---
+++ Example17/MainWindow.xaml.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Windows;
+
+namespace Example17
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            // Event handlers can be assigned either here in the code-behind, or in XAML.
+            //this.btnClickMe.Click += OnClick;
+        }
+
+        // The method below is an event handler, which is
+        // triggered when the linked button is clicked.
+        // Event handlers ALWAYS define two parameters:
+        // - Sender: Reference to the triggering control.
+        // - Event arguments: Event meta-data.
+        private void OnClick(object sender, EventArgs e)
+        {
+            MessageBox.Show("Clicked!");
+        }
+    }
+}
---
+++ Example18/MainWindow.xaml.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Windows;
+
+namespace Example18
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            this.btnSignIn.Click += OnSignIn;
+        }
+
+        private void OnSignIn(object sender, EventArgs e)
+        {
+            string username = this.txtUsername.Text;
+            string password = this.pwdPassword.Password;
+
+            if (username == "john" && password == "smith")
+            {
+                MessageBox.Show("Login successful!", "Success!", MessageBoxButton.OK, MessageBoxImage.Information);
+            }
+            else
+            {
+                MessageBox.Show("Invalid credentials!", "Error!", MessageBoxButton.OK, MessageBoxImage.Error);
+            }
+        }
+    }
+}
